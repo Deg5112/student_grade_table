b64109cd0cbeb3a640c8fa39d5629bf7e206c5e2
davidgoodman <dgoodman@tomferry.com>
2020-02-23 01:27:52 -0800

    assets

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta name="viewport" content="initial-scale=1, user-scalable=no">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <!--<script src="script.js"></script>-->
 
 
