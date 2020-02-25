0f8a2e668655360e799b4eec557bd00e9a214fc5
davidgoodman <dgoodman@tomferry.com>
2020-02-24 18:47:26 -0800

    fix

--- config/database.php
+++ config/database.php
@@ -36,18 +36,18 @@ return [
 
 
     'connections' => [
-        'pgsql' =>  [
+        'pgsql' =>  (new DatabaseUrlParser('DATABASE_URL'))->setDefaults([
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
-        ],
+        ])->parse(),
     ],
 
     /*
